f6f92c2282d4137722e7c5f568fa9661cb02de60
llynn <lynn.felicity@gmail.com>
2013-09-30 10:19:08 -0700

    new updatebeta_RW.cpp

--- updatebeta_RW.cpp
+++ updatebeta_RW.cpp
@@ -9,7 +9,7 @@
 #include <R_ext/Utils.h>
 using namespace std;
 void updatebeta_RW(vector<double>& xmust, vector<double>& xmuut, vector<int>& xn_s, vector<int>& xn_u, int xI, int xK, int xM,
-                     int t1, double xp_var, double sqrt_var1, double sqrt_var2, int xtt, vector<int>& xgammat, 
+                      int K1, int t1, double xp_var, double sqrt_var1, double sqrt_var2, int xtt, vector<int>& xgammat, 
                       Rcpp::IntegerMatrix& xd, Rcpp::NumericMatrix& xybar_s, Rcpp::NumericMatrix& xybar_u, Rcpp::NumericMatrix& pt1, Rcpp::NumericMatrix& pt2, 
                       Rcpp::NumericMatrix& pt3, double xlambda, Rcpp::NumericVector& beta, double xalpha, double xsig_beta1, 
                       double xbeta1, Rcpp::IntegerVector& xAbeta)
@@ -29,7 +29,7 @@ void updatebeta_RW(vector<double>& xmust, vector<double>& xmuut, vector<int>& xn
     for (int p=0; p<xM; p++) {
    
         for (int i=0; i<xI;i++) {
-         for(int k=0; k<xK; k++) {
+         for(int k=0; k<K1; k++) {
            temp = xI*k+i;
              if(xd(k,p)==1) {
                 nik = xn_u[temp]+xn_s[temp];
